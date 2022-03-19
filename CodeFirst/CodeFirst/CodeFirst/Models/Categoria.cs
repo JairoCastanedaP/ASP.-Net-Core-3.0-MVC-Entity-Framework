3730455d9452df75b7a7c63713dbd33e839823b1
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-03-19 17:04:01 -0500

    CodeFirst - Creando Proyecto y Modelo de datos

---
+++ CodeFirst/CodeFirst/CodeFirst/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Models
+{
+    public class Categoria
+    {
+        public int idCategoria { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public bool? estado { get; set; }
+
+        public virtual ICollection<Producto> productos { get; set; }
+    }
+}
