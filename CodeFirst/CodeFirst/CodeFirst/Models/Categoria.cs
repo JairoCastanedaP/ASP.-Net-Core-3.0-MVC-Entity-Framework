77a23b9c854981ac32b63d980f2c51961dd98199
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-03-20 20:22:57 -0500

    Se agregan los DataAnnotations para las categorías y los productos

--- CodeFirst/CodeFirst/CodeFirst/Models/Categoria.cs
+++ CodeFirst/CodeFirst/CodeFirst/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace CodeFirst.Models
     public class Categoria
     {
         public int idcategoria { get; set; }
-        //public string codigo { get; set; }
+        
+        [Required(ErrorMessage ="Ingrese un nombre")]
+        [StringLength(50,MinimumLength =3,ErrorMessage = "El nombre no debe rener más de 50 caracteres, ni menos de 3")]
         public string nombre { get; set; }
+        
+        [StringLength(255,ErrorMessage ="la descripción no debe tener más de 255 caracteres")]
         public string descripcion { get; set; }
         public bool? estado { get; set; }
 
--- CodeFirst/CodeFirst/CodeFirst/Models/Producto.cs
+++ CodeFirst/CodeFirst/CodeFirst/Models/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,28 @@ namespace CodeFirst.Models
     public class Producto
     {
         public int idproducto { get; set; }
+        
+        [Required(ErrorMessage ="Seleccione una categoría")]
         public int idcategoria { get; set; }
+        
+        
+        [StringLength(64,ErrorMessage ="elcódigo no debe tener más de 64 caracteres")]
         public string codigo { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un nombre")]
+        [StringLength(100,MinimumLength =3,ErrorMessage ="el nombre no debe tener más de 100 caracteres ni menos de 3")]
         public string nombre { get; set; }
+        
+        [Required(ErrorMessage ="Ingrese un precio de venta")]
         public decimal precio_venta { get; set; }
+        
+        [Required(ErrorMessage ="Ingrese un stock")]
         public int stock { get; set; }
+
+        [StringLength(255, ErrorMessage = "la descripción no debe tener más de 255 caracteres")]
         public string descripcion { get; set; }
+
+
         public bool? estado { get; set; }
         
         public virtual Categoria categoria { get; set; }
